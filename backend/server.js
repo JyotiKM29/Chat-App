c0806205e944d5e097073329fd8b09740e78ec2a
JyotiKM29 <jyotikumari4442@gmail.com>
2023-07-02 21:20:08 +0530

    Api is working and Chats are fetching

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,27 @@
 const express = require("express");
+const { chats } = require("./data/data");
 
 const app = express();
+
+// checking Api connection
+app.get('/' , (req, res)=>{
+    res.send("Jyoti is placed at 24 LPA 🥳")
+});
+
+
+// fetch data from server
+app.get('/api/chat',(req,res) =>{
+    res.send(chats);
+});
+
+// fetching single data
+app.get('/api/chat/:id',(req,res) =>{
+    // console.log(req);
+    const singleChat = chats.find((c) => {
+        return c._id === req.params.id;
+    });
+    res.send(singleChat);
+});
+
 
 app.listen(8000, console.log("Server is setup"));
